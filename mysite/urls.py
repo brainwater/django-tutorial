bf7cb532f62119ac7c8bad64307263008ec19d28
Blake Rainwater <rainwaterblake@gmail.com>
2013-11-12 14:33:24 -0500

    Add poll detail

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
